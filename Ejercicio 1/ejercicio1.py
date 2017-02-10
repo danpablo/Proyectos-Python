c00b2cc5e9d096385c1f877d75c86fd7b8f6bdc3
danpablo <danpablo@gmail.com>
2017-02-10 15:51:30 -0300

    ejercicio 1
    ejercicio 1

--- Ejercicio 1/ejercicio1.py
+++ Ejercicio 1/ejercicio1.py
@@ -1,1 +1,9 @@
+# -*- coding: utf-8 -*-
+
 print "Hello, world!!"
+print "hello Again"
+print "I like typing this"
+print "This is fun"
+print 'Yay! Printing.'
+print "I'd much rather you 'not'.ñ"
+print 'I "said" do not touch this'
